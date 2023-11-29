b7352e5b1428ff250e4b422c9c0db57bc696f6a0
ZinoChan <devzinochan@gmail.com>
2023-11-29 10:24:44 +0100

    refactor

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => ({
-      status: 200,
-      value: values,
-    }))
-    .catch((error) => ({
-      status: error.status,
-      value: error.message,
-    }));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })),
+  );
 }
