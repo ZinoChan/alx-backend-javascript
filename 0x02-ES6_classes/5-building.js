462c5e4b300b2bf0812596bb12e3ff1143eb8748
ZinoChan <devzinochan@gmail.com>
2023-11-30 13:57:05 +0100

    fix: add setter method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,20 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft === 'number') this._sqft = sqft;
-    if (
-      this.constructor === Building && this.evacuationWarningMessage === undefined) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
-    }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
   }
 }
