4e619cd2ff3c11376f4ee4d0b43d5e2c3f2fd9d7
ZinoChan <devzinochan@gmail.com>
2023-11-30 13:43:58 +0100

    fix issues

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') this._name = name;
+    else throw new TypeError('Name must be a string.');
     if (typeof length === 'number') this._length = length;
+    else throw new TypeError('Length must be a number.');
     if (students instanceof Array) this._students = students;
+    else throw new TypeError('Students must be an array.');
   }
 
   get name() {
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,15 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft === 'number') this._sqft = sqft;
+    if (
+      this.constructor === Building && this.evacuationWarningMessage === undefined) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
   }
 }
