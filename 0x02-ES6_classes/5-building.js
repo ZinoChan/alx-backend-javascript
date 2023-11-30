8bc1b3b9d013edeb8a1f5c6193ff22bfe5522078
ZinoChan <devzinochan@gmail.com>
2023-11-30 14:04:17 +0100

    refactor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error(
+    if (typeof sqft === 'number') this._sqft = sqft;
+    if (
+      this.constructor !== Building && this.evacuationWarningMessage === undefined
+    ) {
+      throw Error(
         'Class extending Building must override evacuationWarningMessage',
       );
     }
-    if (typeof sqft === 'number') this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    this._sqft = newSqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
   }
 }
