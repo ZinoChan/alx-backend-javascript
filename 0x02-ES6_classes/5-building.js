9bbf39d082262f6d4804a3b47b3ae9a8e1d4a4a3
ZinoChan <devzinochan@gmail.com>
2023-11-30 13:23:31 +0100

    finish tasks

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft === 'number') this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
+  }
+}
