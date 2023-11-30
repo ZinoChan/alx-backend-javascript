2aabe879e6bccc36215c7f40ca236c84365491c1
ZinoChan <devzinochan@gmail.com>
2023-11-30 14:01:06 +0100

    fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,10 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
     if (typeof sqft === 'number') this._sqft = sqft;
   }
 
