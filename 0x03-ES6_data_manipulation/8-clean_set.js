8cc044ecadf81785c58bdf2fc68e7943928a38e6
ZinoChan <devzinochan@gmail.com>
2023-12-05 09:49:37 +0100

    check for undefined values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) return '';
   return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
+    .filter((value) => (value ? value.startsWith(startString) : ''))
+    .map((value) => (value ? value.slice(startString.length) : ''))
     .join('-');
 }
